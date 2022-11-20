49c8212fa434977b6ea2092e561893ce61a7bf8f
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2022-11-20 10:59:56 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,6 @@
+a= int(input("enter the number to see table of: ")) #take input for the table of number a
+i=0
+x=(int(input("enter the number till where you want your table to run: "))) #take another input for range of table
+for i in range(1,x+1): #use loop
+    x=i*a #create formula
+    print(a,"*",i,"=",x) #print result
