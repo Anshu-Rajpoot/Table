6ab0815d6fcb7f7f0ab530feb019b6e88ed1beb2
Anshu Rajpoot <107042248+Anshu-Rajpoot@users.noreply.github.com>
2025-09-30 17:14:56 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,6 +1,7 @@
-a= int(input("enter the number to see table of: ")) #take input for the table of number a
+a= int(input("Enter the number to see table of: ")) #take input for the table of number a
 i=0
-x=(int(input("enter the number till where you want your table to run: "))) #take another input for range of table
+x=(int(input("Enter the number till where you want your table to run: "))) #take another input for range of table
 for i in range(1,x+1): #use loop
     x=i*a #create formula
     print(a,"*",i,"=",x) #print result
+
